b4b2563ecea393849b7c74a0cf2740c78408be2a
Reider2k20 <115879080+Reider2k20@users.noreply.github.com>
2022-10-30 14:41:14 +0200

    Delete p6_2.py

--- p6_2.py
+++
@@ -1,9 +0,0 @@
-try:
-    try:
-        print("Start")
-        print(Hello)
-        print("End. No error")
-    except SyntaxError:
-        print("Wrong Syntax")
-except NameError as error:
-    print(error)
